68a03d50a73a2d9d16e3abfab351942443023b71
insectfow <ehdgus441@naver.com>
2022-11-16 18:19:59 +0900

    useState study!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
 
 function App() {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Edit <code>src/App.js</code> and save to reload. { item }
+      </p>
+      <button onClick={incrementItem}>incrementItem</button>
+      <button onClick={decrementItem}>decrementItem</button>
     </div>
   );
 }
+
+// class AppUgly extends React.Component {
+//   state = {
+//     item: 1
+//   };
+//   render() {
+//     const { item } = this.state;
+//     return (
+//       <div className="App">
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload. { item }
+//         </p>
+//         <button onClick={this.incrementItem}>incrementItem</button>
+//         <button onClick={this.decrementItem}>decrementItem</button>
+//       </div>
+//     );
+//   };
+//   incrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item + 1
+//       };
+//     })
+//   };
+//   decrementItem = () => {
+//     this.setState(state => {
+//       return {
+//         item: state.item - 1
+//       };
+//     })
+//   };
+// }
 
 export default App;
