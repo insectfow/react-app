9daa5a9a9178ab70909cf3a294e3ad6c5d979e5d
insectfow <ehdgus441@naver.com>
2022-11-24 14:49:12 +0900

    useFullscreen study!

--- src/useFullscreen.js
+++ src/useFullscreen.js
@@ -1,7 +1,4 @@
-import React, { useEffect, useRef, useState, } from 'react';
-import './App.css';
-
-const useFullscreen = (callback) => {
+export const useFullscreen = (callback) => {
   const element = useRef();
   const runCb = (isFull) => {
       if (callback && typeof callback === 'function') {
@@ -37,23 +34,3 @@ const useFullscreen = (callback) => {
   }
   return {element, triggerFull, exitFull}
 }
-
-
-const App = () => {
-  const onFullS = (isFull) => {
-    console.log(isFull ? 'full' : 'short');
-  }
-  const { element, triggerFull, exitFull } = useFullscreen(onFullS);
-  return (
-    <div className="App" >
-      <div ref={element}>
-        <img  src="https://images.unsplash.com/photo-1669147528483-5b5b4493cbe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"></img>
-        <button onClick={exitFull}>exitFull</button>
-
-      </div>
-      <button onClick={triggerFull}>makeFullscreen</button>
-    </div>
-  );
-}
-
-export default App;
